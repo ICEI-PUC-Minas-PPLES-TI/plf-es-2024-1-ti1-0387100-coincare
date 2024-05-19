74b39bd54956371dac97e832b872e7344b6f24d6
Italo Vitorino <italo.vitorinobs@hotmail.com>
2024-05-19 17:20:40 -0300

    feat: ordenar dados

--- codigo/src/js/movement-history.js
+++ codigo/src/js/movement-history.js
@@ -7,12 +7,14 @@
 // Exibir mensagem caso não for encontrado nenhum registro - OK
 // Esconder tabela antes do usuário pesquisar
 // Totalizador
+// Exibir os dados formatados (valor e data)
 
 const quickFiltersBtn = document.querySelectorAll('.c-button--quick-filter');
 const inputsDateElement = document.querySelectorAll('.c-input-date');
 const searchBtn = document.querySelector('.c-button--search');
 const dataNotFound = document.querySelector('.main__data-not-found');
 const tbodyElement = document.querySelector('tbody');
+const orderBy = document.querySelector('#order-by');
 
 const URL_BASE = 'http://localhost:3000/movimentacoes';
 
@@ -28,10 +30,12 @@ function getCurrentDateInUTC() {
   ));
 }
 
-function setDateInputsAndFetch(startDate, endDate) {
+async function setDateInputsAndFetch(startDate, endDate) {
   inputsDateElement[0].value = dateFns.format(startDate, 'yyyy-MM-dd');
   inputsDateElement[1].value = dateFns.format(endDate, 'yyyy-MM-dd');
-  getAll();
+  const data = await getAll();
+  const filteredData = filterData(data);
+  populateTable(filteredData);
 }
 
 function addQuickFilterEvent(button, startDateFn, endDateFn) {
@@ -50,11 +54,9 @@ function createAndAppendTd(parent, text) {
 }
 
 function populateTable(movements) {
-
   tbodyElement.innerText = '';
 
   movements.forEach(item => {
-
     const tr = document.createElement('tr');
 
     createAndAppendTd(tr, item.id);
@@ -85,32 +87,56 @@ function filterData(data) {
     return;
   }
 
-  populateTable(filteredData);
+  return filteredData;
+}
+
+function orderByF(typeOrder, data) {
+  switch (typeOrder) {
+    case 'transaction-date':
+      data.sort((a, b) => {
+        const dateA = new Date(`${a.mes_ano}-${a.dia}`);
+        const dateB = new Date(`${b.mes_ano}-${b.dia}`);
+        return dateB - dateA;
+      });
+      break;
+
+    case 'highest-value':
+      data.sort((a, b) => b.valor - a.valor);
+      break;
+
+    case 'lower-value':
+      data.sort((a, b) => a.valor - b.valor);
+      break;
+
+    case 'description':
+      data.sort((a, b) => a.descricao.localeCompare(b.descricao));
+      break;
+    default:
+  }
+
+  populateTable(data);
 }
 
 async function getAll() {
-
   try {
-
     const response = await fetch(URL_BASE);
 
     if (!response.ok) throw new Error('Não foi possível carregar os dados!');
 
     const movements = await response.json();
 
-    filterData(movements);
-
+    return movements;
   } catch (error) {
-
     console.log(error);
-
   }
 }
 
-searchBtn.addEventListener("click", () => {
+searchBtn.addEventListener('click', async () => {
   const allDatesAreValid = Array.from(inputsDateElement).every(date => dateIsValid(date));
   if (allDatesAreValid) {
-    getAll();
+    const data = await getAll();
+    const filteredData = filterData(data);
+    populateTable(filteredData);
   } else {
     alert('Data(s) inválidas!');
   }
@@ -120,3 +146,12 @@ addQuickFilterEvent(quickFiltersBtn[0], date => date, date => date);
 addQuickFilterEvent(quickFiltersBtn[1], dateFns.startOfWeek, dateFns.endOfWeek);
 addQuickFilterEvent(quickFiltersBtn[2], dateFns.startOfMonth, dateFns.endOfMonth);
 addQuickFilterEvent(quickFiltersBtn[3], dateFns.startOfYear, dateFns.endOfYear);
+
+orderBy.addEventListener('change', async () => {
+  const data = await getAll();
+  const filteredData = filterData(data);
+
+  if (data) {
+    orderByF(orderBy.value, filteredData);
+  }
+})
