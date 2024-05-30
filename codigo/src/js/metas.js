09c016e023c50c8aef2180be811f329b1f3b9929
Italo Vitorino <italo.vitorinobs@hotmail.com>
2024-05-30 00:58:59 -0300

    feat: criação de metas no arquivo db.json

--- codigo/src/js/metas.js
+++ codigo/src/js/metas.js
@@ -1,4 +1,51 @@
-const progress = document.querySelector('.card__progress')
+const descricao = document.querySelector('#descricao');
+const saldoInicial = document.querySelector('#saldo-inicial');
+const metaC = document.querySelector('#meta');
+const alcancarEm = document.querySelector('#alcancar-em');
+const observacao = document.querySelector('#observacoes');
+const salvarBtn = document.querySelector('.c-button-save');
+
+const urlBase = 'http://localhost:3000';
+
+function criarMeta(meta) {
+  fetch(`${urlBase}/metas`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(meta),
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log('meta inserida com sucesso!');
+    })
+    .catch(error => {
+      console.error('erro ao inserir meta');
+    })
+}
+
+salvarBtn.addEventListener('click', () => {
+  const campoDescricao = descricao.value;
+  const campoSaldoInicial = saldoInicial.value;
+  const campoMeta = metaC.value;
+  const campoAlcancarEm = alcancarEm.value;
+  const campoObservacao = observacao.value;
+
+  const meta = {
+    descricao: campoDescricao,
+    saldoInicial: campoSaldoInicial,
+    meta: campoMeta,
+    alcancarEm: campoAlcancarEm,
+    status: 'Em andamento',
+    observacao: campoObservacao,
+    movimentacoes: []
+  };
+
+  criarMeta(meta);
+})
+
+// Círculo de progresso
+const progress = document.querySelector('.card__progress');
 
 var bar = new ProgressBar.Circle(progress, {
   color: '#717171',
